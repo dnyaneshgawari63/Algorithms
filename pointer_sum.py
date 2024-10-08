91b92bcf9b772e6dc983eb05e351d96b816a651b
Dnyanesh <dnyaneshgawari63@gmail.com>
2024-10-08 23:23:52 +0530

    updt

--- pointer_sum.py
+++ pointer_sum.py
@@ -1,8 +1,15 @@
 list = [10,11,12,13,14,15,16,17,18,19,20]
+target  = 25
 
 left = list[0]
-right = list[-2]
+right = list[-1]
 
-sum  = left + right
-
-print(sum)
+while left < right:
+    cur_sum = left + right
+    if cur_sum == target:
+        print(f"found {left} and {right}")
+        break
+    elif cur_sum < target:
+        left = left + 1
+    else:
+        right = right - 1

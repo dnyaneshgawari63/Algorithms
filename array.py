262cbeb7cfecf5acfffb796f1d481b7e1ce69639
Dnyanesh <dnyaneshgawari63@gmail.com>
2024-10-12 23:55:47 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,9 +1,9 @@
-def find(arr, target):
-    for x in range(len(arr)):
-        if arr[x] == target:
+def find(array, target):
+    for x in range(len(array)):
+        if array[x] == target:
             return x
     return -1
 
-arr = [10,11,12,13,14,15,16,17,18,19,20] 
+array = [10,11,12,13,14,15,16,17,18,19,20] 
 target = 14
-print(find(arr, target))
+print(find(array, target))

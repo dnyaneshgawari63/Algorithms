2b51683e71bc93c6c27e60c146d528b4f5a3b08c
Dnyanesh <dnyaneshgawari63@gmail.com>
2024-10-13 23:59:05 +0530

    updt

--- func.py
+++ func.py
@@ -1,1 +1,20 @@
 # def two pointer for sum of any given list array
+
+def find_pair():
+    list = [1,2,3,4,5]
+    target = 6
+    # print(list)
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        cur_sum = list[left] + list[right]
+        if cur_sum == target:
+            print(f"found {list[left]} and {list[right]}")
+            break
+        elif cur_sum < target:
+            left = left + 1
+        else:
+            right = right - 1
+
+find_pair()
+

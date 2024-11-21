d9f5de5a17b6e5faea052f4385bd89918ff69dc8
Dnyanesh <dnyaneshgawari63@gmail.com>
2024-11-21 22:36:34 +0530

    Update and rename Iterator.py to generator.py

--- Iterator.py
+++
@@ -1,9 +0,0 @@
-
-def square_iterator(limit):
-  x = 0
-  while x < limit:
-    yield x*x
-    x = x+1
-
-for num in sqaure_iterator(5):
-  print(num)
---
+++ generator.py
@@ -0,0 +1,10 @@
+# In generators, we use the keywork yeild
+# its memory efficient and good when we have huge amount of dataset
+def square_iterator(limit):
+  x = 0
+  while x < limit:
+    yield x*x
+    x = x+1
+
+for num in square_iterator(5):
+  print(num)

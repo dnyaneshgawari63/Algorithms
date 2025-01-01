b11ec0db50765045be510e5bf7c11ca0fdbe2c85
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-01-01 23:24:07 +0530

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -2,17 +2,24 @@ class Animal:
   def sound(self):
     pass
 
-class Dog:
+class Dog(Animal):
   def sound(self):
     return "bhau bhau"
 
-class Cat:
+class Cat(Animal):
   def sound(self):
     return "meaao"
 
-class Bird:
+class Bird(Animal):
   def sound(self):
     return "chu chu chu"
 
 def make_sound(animal):
-  print(animal.sound)
+  print(animal.sound())
+
+dog = Dog()
+cat = Cat()
+bird = Bird()
+
+# print result to test above code using below code line
+make_sound(cat)

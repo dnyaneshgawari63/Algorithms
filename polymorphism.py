f14d28cf793f84e68bb41f9b4e45a6e43adab885
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-01-01 23:21:09 +0530

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,1 +1,18 @@
+class Animal:
+  def sound(self):
+    pass
 
+class Dog:
+  def sound(self):
+    return "bhau bhau"
+
+class Cat:
+  def sound(self):
+    return "meaao"
+
+class Bird:
+  def sound(self):
+    return "chu chu chu"
+
+def make_sound(animal):
+  print(animal.sound)

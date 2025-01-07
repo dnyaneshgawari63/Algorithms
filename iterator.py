48c7ce3f38a41e5f1b8e8286653a497f22869d73
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-01-07 23:28:23 +0530

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -1,1 +1,11 @@
-# iterators in function 
+# iter inbuilt command iter the sequence of string or items in a list till the last element itered when we use next word at the time of print.
+
+abc = ('danny', 'bunny', 'money')
+
+res = iter(abc)
+
+print(next(res))
+print(next(res))
+print(next(res))
+
+# if we keep printing print(next(res)) on fourth line then, the iteration gets stopped after sequences of elemenst are finnished.

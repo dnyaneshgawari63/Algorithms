1258cb6cf818e515bfa17141231f1d52375cb8d5
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-01-24 23:19:03 +0530

    Update array_lowest_number.py

--- array_lowest_number.py
+++ array_lowest_number.py
@@ -1,9 +1,10 @@
-def lowest_val():
+# Here we are trying to find the lowest value in the below array
+def lowest_val(): 
   arr = [123,324,34,23,423,23543,65]
-  lowest = arr[0]
+  lowest = arr[0]  # here setting a lowest at zero
   for ar in arr:
-    if ar < lowest:
+    if ar < lowest: 
       lowest = ar
   return lowest
-print(lowest_val())
+print(lowest_val()) 
       

bff2fbf166bfa36c9f675065e5f0190063ea2c3d
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-01-28 23:17:46 +0530

    Update decorators2.py

--- decorators2.py
+++ decorators2.py
@@ -1,10 +1,10 @@
-def calculation(generic):
+def calculation(anything):
   def wrapper(x, y):
     return x ** y
   return wrapper
 
-@calculation
-def nums():
+@calculation 
+def nums(): # decorator takes wrapper function as argument
   return
 
 print(nums(3,4))

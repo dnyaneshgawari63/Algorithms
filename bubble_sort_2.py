f2402f885f35eb743580294bedd4b9694a167b17
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-02-16 23:45:57 +0530

    Update bubble_sort_2.py

--- bubble_sort_2.py
+++ bubble_sort_2.py
@@ -3,7 +3,7 @@
 list = [5,1,4,2,7,6,9,8,10]
 
 for x in range(len(list)-1):
-  for y in range(len(list)-x-1:
+  for y in range(len(list)-x-1):
     if list[y] > list[y+1]:
       list[y], list[y+1] = list[y+1], list[y]
 

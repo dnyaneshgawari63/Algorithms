9753acf01a2153138691546f23cb06ac7404832f
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-02-16 23:44:55 +0530

    Update bubble_sort_2.py

--- bubble_sort_2.py
+++ bubble_sort_2.py
@@ -1,10 +1,10 @@
 # simplified version
 
-class Sort:
-  def __init__(self, list):
-    self.list = list
-    n = len(self.list)
-    for x in len(range(n-1):
-      swap = 0
-      for y in len(range(x-n-1):
+list = [5,1,4,2,7,6,9,8,10]
 
+for x in range(len(list)-1):
+  for y in range(len(list)-x-1:
+    if list[y] > list[y+1]:
+      list[y], list[y+1] = list[y+1], list[y]
+
+print(list)

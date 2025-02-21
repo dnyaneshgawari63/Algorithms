28b1b63750c001cc5127bfea860342a77780c86c
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-02-21 23:18:05 +0530

    Update and rename selection_sort.py to array_selection_sort.py

--- array_selection_sort.py
+++ array_selection_sort.py
@@ -1,4 +1,7 @@
 # Here we add the lowest value before the greter value in the array
 
 def select_sort():
-  
+  n =len(array)
+  for x in range(n-1):
+    min_index = x
+    for y in range(x+1, n):

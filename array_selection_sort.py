f7dca020a9439931f6c06082e9f7d308c4e6ca17
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-02-23 17:40:03 +0530

    Update array_selection_sort.py

--- array_selection_sort.py
+++ array_selection_sort.py
@@ -1,7 +1,16 @@
 # Here we add the lowest value before the greter value in the array
 
+array = [12,43,2,44,5,6,7,53,3,6,6]
+
 def select_sort():
-  n =len(array)
+  n = len(array)
   for x in range(n-1):
     min_index = x
     for y in range(x+1, n):
+      if array[y] < array[min_index]:
+        min_index = y
+    min_value = array.pop(min_index)
+    array.insert(x, min_value)
+  return array
+
+select_sort()

f167d86a7328c7e6ffc4b6bde14a215da79aeb23
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-03-01 23:12:23 +0530

    Update array_insertion_sort.py

--- array_insertion_sort.py
+++ array_insertion_sort.py
@@ -3,3 +3,9 @@ def insertsort(array):
   for x in range(1, n):
     insert_ind = x
     current_val = array.pop(x)
+    for y in range(x-1, -1, -1):
+      if array[y] > current_val:
+        insert_ind = y
+    array.insert(insert_ind, current_val)
+
+print(array)

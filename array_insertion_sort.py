43bfc0fff916895bfadc60be626848c268c6d056
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-03-03 12:25:39 +0530

    Update array_insertion_sort.py

--- array_insertion_sort.py
+++ array_insertion_sort.py
@@ -1,14 +1,14 @@
-def insertsort(array):
-  n = len(array)
-  for x in range(1, n):
-    insert_ind = x
-    current_val = array.pop(x)
-    for y in range(x-1, -1, -1):
-      if array[y] > current_val:
-        insert_ind = y
-    array.insert(insert_ind, current_val)
+# sort array using insert sort method
+def abc(array):
+    n = len(array)
+    for x in range(1, n):
+        insert_ind = x
+        current_val = array.pop(x)
+        for y in range(x-1, -1, -1):
+            if array[y] > current_val:
+                insert_ind = y
+        array.insert(insert_ind, current_val)
     return array
 
-
-array = [123,324,35,56,5324,42,6,56,54324,43,45]
-insertsort(array)
+array = [213,543,53,23,34,2133,423,45,334,23,343,5454,4,46556]
+abc(array)

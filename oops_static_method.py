13a58f0f6901298afc6a14836b67e461b7f74966
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-03-07 23:59:25 +0530

    Update oops_static_method.py

--- oops_static_method.py
+++ oops_static_method.py
@@ -1,2 +1,4 @@
 # static method is a built in method where we dont need self parameter and init method to create instance and call it. Instead static method directly calls the respective function.
 
+
+class 

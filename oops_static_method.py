2516cfd0687107c1df6f55cdd2fac29fade3cee4
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-03-08 23:04:21 +0530

    Update oops_static_method.py

--- oops_static_method.py
+++ oops_static_method.py
@@ -1,15 +1,15 @@
 # static method is a built in method where we dont need self parameter and init method to create instance and call it. Instead static method directly calls the respective function.
 
 class Calc:
-@staticmethod
-def add(a,b):
-  return a * b
+  @staticmethod
+  def add(a,b):
+    return a * b
 
-@staticmethod
-def subs(a,b):
-  return a - b
+  @staticmethod
+  def subs(a,b):
+    return a - b
 
-result = add(23,34)
-result2 = subs(123-10)
+result = Calc.add(23,34)
+result2 = Calc.subs(123-10)
 
 

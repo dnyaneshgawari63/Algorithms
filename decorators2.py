376cbd32a6e526ee1cf7811457de022ab256cdd8
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-06-01 00:00:20 +0530

    Update decorators2.py

--- decorators2.py
+++ decorators2.py
@@ -10,7 +10,8 @@ def nums(): # decorator takes wrapper function as argument
   return
 
 @calculation
-def nums2():
+def nums2(a, b):
+  return a + b
 
 print(nums(3,4))  #it prints the result from functionality assigned to variables in main function 
 print(nums(2.3, 4))

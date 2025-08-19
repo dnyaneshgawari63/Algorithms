504fdccf079d414412a725be1c74b1a1dbfe11af
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-08-20 00:01:47 +0530

    Update array_bubble_sort.py

--- array_bubble_sort.py
+++ array_bubble_sort.py
@@ -2,14 +2,6 @@ array = [123,321,324,324,243,56,87,934,789,980]
 
 n = len(array)
 
-for x in range(n-1):
-  swapped = False
-  for y in range(n-x-1):
-    if array[y] > array[y+1]:
-      array[y], array[y+1] = array[y+1], array[y]
-      swapped = True
-  if not swapped:
-      break
-
-print(array)
+for x in n-1:
+  
 

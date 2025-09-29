34b8dbdfa8c18fc7998a840760ee33a174cc26a0
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-09-30 02:13:45 +0530

    Update array.py

--- array.py
+++ array.py
@@ -64,9 +64,11 @@ print(a)
 # Example 8
 # out of index error handeling 
 a = [1,2,3,4,5,6,7,8,9,10]
-for x in range(1, len(a), 2):
-    a[x] = a[x+2]
+# Example 8
+# out of index error handeling 
+a = [1,2,3,4,5,6,7,8,9,10]
+try:
+    for x in range(1, len(a), 2):
+        a[x] = a[x+2]
 except Exception as e:
-
-# 
-
+    e

cf8a9fc28f88c3f3c4e1855d1e0988608d8b1501
Dnyanesh <dnyaneshgawari63@gmail.com>
2025-11-03 23:59:25 +0530

    Update array.py

--- array.py
+++ array.py
@@ -85,5 +85,5 @@ abc.append(x)
 # Dictionary methods
 abc = {}
 abc["Name"] = "Bhalu" # name becomes key and Bhalu is value in abc dictionary form
-abc[("1,2,3")] = 1000 # In dictionary only tuple can add multiple keys or values as tuple can notbe modified or rearranged like set dict and list
+abc[("1,2,3")] = 1000 # In dictionary only tuple can add multiple keys or values as tuple can notbe modified
 

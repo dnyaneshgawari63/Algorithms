14d7a7e024a32ab6e668d8256b5c3fc45a8d19c9
Dnyanesh <dnyaneshgawari63@gmail.com>
2026-01-23 00:11:48 +0530

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,9 +1,15 @@
 # decorators are the extension to the previously written code functionality 
-def decorator(func):
-  def wrapper():
-    print("abc123")
-    result = func()
-    print("cde456")
-    return result
+
+def calc_decorator(func):
+  def wrapper(a, b):
+    print(a + b)
+    print(a - b)
+    return func(a,b)
   return wrapper
 
+@calc_decorator
+def operate(a, b):
+  pass
+
+operate(10,20)
+    
